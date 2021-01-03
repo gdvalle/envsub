5a86c792718465e804fbca6c811b0b584d1bfe4e
Greg Dallavalle <greg.dallavalle@leadpages.com>
2021-01-03 11:42:19 -0600

    Use GitHub Actions

--- tests/tests.rs
+++ tests/tests.rs
@@ -6,57 +6,53 @@ use subprocess::Exec;
 
 const NAME: &'static str = "envsub";
 
-/// Returns the path to the executable.
-#[cfg(not(windows))]
-pub fn cargo_exe(name: &str) -> PathBuf {
-    let root = cargo_root();
-    let path = root.join(name);
-    if !path.is_file() {
-        // Looks like a recent version of Cargo changed the cwd or the
-        // location of the test executable.
-        root.join(format!("../{}", name))
-    } else {
-        path
-    }
-}
-
-/// Returns the path to the executable.
-#[cfg(windows)]
-pub fn cargo_exe(name: &str) -> PathBuf {
-    let root = cargo_root();
-    let path = root.join(format!("{}.exe", name));
-    if !path.is_file() {
-        // Looks like a recent version of Cargo changed the cwd or the
-        // location of the test executable.
-        root.join(format!("../{}.exe", name))
-    } else {
-        path
-    }
-}
-
 pub fn cargo_root() -> PathBuf {
     env::current_exe()
         .unwrap()
         .parent()
         .expect("failed locating exe dir")
+        .parent()
+        .expect("failed locating parent exe dir")
         .to_path_buf()
 }
 
-fn exe(vars: &str) -> Exec {
-    let exe = cargo_exe(NAME);
-    let exe_str = exe.to_str().unwrap();
-    // Abuse shell method until cmd has env var support.
-    let cmd = format!("{} {}", vars, exe_str);
-    Exec::shell(cmd)
+/// Repurposed from BurntSushi/ripgrep/tests/util.rs.
+/// This determines the proper executable when we're in a cross compiler.
+fn cross_runner() -> Option<String> {
+    for (k, v) in std::env::vars_os() {
+        let (k, v) = (k.to_string_lossy(), v.to_string_lossy());
+        if !(k.starts_with("CARGO_TARGET_") && k.ends_with("_RUNNER")) {
+            continue;
+        }
+        return Some(v.into_owned());
+    }
+    None
+}
+
+/// Returns the path to the executable.
+pub fn cargo_exe(name: &str) -> PathBuf {
+    cargo_root().join(format!("{}{}", name, env::consts::EXE_SUFFIX))
+}
+
+fn exe() -> Exec {
+    let bin_path = cargo_exe(NAME);
+    return match cross_runner() {
+        None => Exec::cmd(bin_path),
+        Some(runner) => Exec::shell(format!(
+            "{} {}",
+            runner,
+            bin_path.into_os_string().into_string().unwrap()
+        )),
+    };
 }
 
 #[test]
 fn output_without_replacements() {
     let expected = "foo bar baz";
-    let output = exe("")
+    let output = exe()
         .stdin(expected)
         .capture()
-        .expect("failed building cmd")
+        .expect("failed running test")
         .stdout_str();
     assert_eq!(output, expected);
 }
@@ -65,10 +61,11 @@ fn output_without_replacements() {
 #[test]
 fn output_with_one_replacement() {
     let expected = "foo foo baz";
-    let output = exe("VAR=foo")
+    let output = exe()
+        .env("VAR", "foo")
         .stdin("foo %VAR% baz")
         .capture()
-        .expect("failed building cmd")
+        .expect("failed running test")
         .stdout_str();
     assert_eq!(output, expected);
 }
@@ -77,8 +74,6 @@ fn output_with_one_replacement() {
 #[test]
 fn multiple_inputs() {
     // Test data from kubernetes var expansion proposal doc.
-    let cmd_prefix = "ENVSUB_PREFIX='$(' ENVSUB_SUFFIX=')' VAR_A=A VAR_B=B VAR_C=C VAR_EMPTY=''";
-    println!("{}", cmd_prefix);
     let table = vec![
         ("$(VAR_A)", "A"),
         ("___$(VAR_B)___", "___B___"),
@@ -108,10 +103,18 @@ fn multiple_inputs() {
         ("$(foo$$var)", "$(foo$$var)"),
     ];
     for (input, expected_output) in table {
-        let output = exe(cmd_prefix)
+        let output = exe()
+            .env_extend(&[
+                ("ENVSUB_PREFIX", "$("),
+                ("ENVSUB_SUFFIX", ")"),
+                ("VAR_A", "A"),
+                ("VAR_B", "B"),
+                ("VAR_C", "C"),
+                ("VAR_EMPTY", ""),
+            ])
             .stdin(input)
             .capture()
-            .expect("failed building cmd")
+            .expect("failed running test")
             .stdout_str();
         assert_eq!(output, expected_output);
     }
